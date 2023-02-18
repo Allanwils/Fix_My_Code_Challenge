75f3f3085efdf644783fb860f6371600a29c6b33
Allan Masiga <zionalehebrew@gmail.com>
2023-02-18 03:50:40 -0800

    fix

--- 0x00-challenge/4-delete_dnodeint/lists.h
+++ 0x00-challenge/4-delete_dnodeint/lists.h
@@ -1,5 +1,5 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stddef.h>
 
@@ -14,14 +14,15 @@
  */
 typedef struct dlistint_s
 {
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
 } dlistint_t;
 
+/* function prototypes */
 size_t print_dlistint(const dlistint_t *h);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 void free_dlistint(dlistint_t *head);
 
-#endif
+#endif /* LISTS_H */
--- 0x00-challenge/4-delete_dnodeint/print_dlistint.c
+++ 0x00-challenge/4-delete_dnodeint/print_dlistint.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "lists.h"
+#include <lists.h>
 
 /**
  * print_dlistint - Prints a doubly linkedlist of integers
